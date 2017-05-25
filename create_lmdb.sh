28ca56b7df12d320a11644afe2d7becb3fcf9f9e
abby <sw62190>
2017-05-25 15:13:28 -0500

    Script for creating the lmdb from the triplet txt files

--- create_lmdb.sh
+++ create_lmdb.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+
 rm -r /project/focus/datasets/tc_tripletloss/small_triplet_test_lmdb
 $CAFFE_ROOT/build/tools/convert_imageset --resize_height 227 --resize_width 227 / /project/focus/datasets/tc_tripletloss/small_test_triplets.txt /project/focus/datasets/tc_tripletloss/small_triplet_test_lmdb
+
 rm -r /project/focus/datasets/tc_tripletloss/small_triplet_train_lmdb
 $CAFFE_ROOT/build/tools/convert_imageset --resize_height 227 --resize_width 227 / /project/focus/datasets/tc_tripletloss/small_train_triplets.txt /project/focus/datasets/tc_tripletloss/small_triplet_train_lmdb
+
 rm -r /project/focus/datasets/tc_tripletloss/triplet_test_lmdb
 $CAFFE_ROOT/build/tools/convert_imageset --resize_height 227 --resize_width 227 / /project/focus/datasets/tc_tripletloss/test_triplets.txt /project/focus/datasets/tc_tripletloss/triplet_test_lmdb
+
 rm -r /project/focus/datasets/tc_tripletloss/triplet_train_lmdb
 $CAFFE_ROOT/build/tools/convert_imageset --resize_height 227 --resize_width 227 / /project/focus/datasets/tc_tripletloss/train_triplets.txt /project/focus/datasets/tc_tripletloss/triplet_train_lmdb
