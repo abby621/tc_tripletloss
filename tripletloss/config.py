f49dd31452bfc4b4fad746cce0b373415480893f
abby <sw62190>
2017-05-25 09:05:37 -0500

    Updating paths for new repo

--- tripletloss/config.py
+++ tripletloss/config.py
@@ -13,8 +13,10 @@ if WHICH_DATASET == 'tc':
     CROP_SZ = 227
     TRAIN_BATCH_SIZE = 30
     TEST_BATCH_SIZE = 30
+    # GLOG_log_dir=/project/focus/abby/tc_tripletloss/models/logs/tc/ $CAFFE_ROOT/build/tools/caffe train -solver /project/focus/abby/tc_tripletloss/models/solver.prototxt -weights /project/focus/abby/tc_tripletloss/models/alexnet_places365.caffemodel -gpu 0
 elif WHICH_DATASET == 'mnist':
     TARGET_SIZE = 28
     CROP_SZ = 28
     TRAIN_BATCH_SIZE = 270
     TEST_BATCH_SIZE = 270
+    # GLOG_log_dir=/project/focus/abby/tc_tripletloss/models/logs/mnist/ $CAFFE_ROOT/build/tools/caffe train -solver /project/focus/abby/tc_tripletloss/models/solver.prototxt -weights /project/focus/abby/tc_tripletloss/models/mnist_pretrained.caffemodel -gpu 0
